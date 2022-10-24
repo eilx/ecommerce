9fdbba4dccd8631a3662a099fd63cb56d3d5f35c
Andrew Thompson <eil23@pm.me>
2022-10-24 16:16:34 +0100

    Implement useProductCategories hook

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -10,3 +10,14 @@ export const useProducts = () => {
 
 	return data
 }
+
+/** Returns a list of valid product categories */
+export const useProductCategories = () => {
+	const apic = `${api}/categories`
+	const { data } = useSWR(apic, () => fetch(apic)
+		.then(response => response.json() as Promise<string[]>)
+		.then(cats => cats.map(category => category.toUpperCase()))
+	)
+
+	return data
+}
