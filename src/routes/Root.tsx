4d1d0120be88f5113174f810a18da99b4bcd0d94
Andrew Thompson <eil23@pm.me>
2022-10-24 12:47:01 +0100

    Add styling to Root (header)

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,8 +1,10 @@
 import { Link, Outlet } from 'react-router-dom'
 
+import css from './css/Root.module.css'
+
 export default function Root () {
 	return <>
-		<header>
+		<header className={css.header}>
 			<nav><ul>
 				<li><Link to='/'>Home</Link></li>
 				<li><Link to='/shop'>Shop</Link></li>
